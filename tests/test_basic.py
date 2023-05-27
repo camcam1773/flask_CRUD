995fcc221b82eb92b6328782c952121346cca122
MC <camcam1773@users.noreply.github.com>
2023-05-27 15:56:27 +0200

    Use reponse.text which returns string instead of byte

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,35 +1,35 @@
 def test_basic(client):
-    response = client.get('/')
-    assert b'Basic CRUD App with Python Flask' in response.data
-    assert b"Nothing yet..." in response.data
-    assert b'Delete' not in response.data
-    assert b'Update' not in response.data
+    response = client.get("/")
+    assert "Basic CRUD App with Python Flask" in response.text
+    assert "Nothing yet..." in response.text
+    assert "Delete" not in response.text
+    assert "Update" not in response.text
 
 
 def poster(client, content: str):
-    return client.post('/', data=dict(content=content), follow_redirects=True)
+    return client.post("/", data=dict(content=content), follow_redirects=True)
 
 
 def deleter(client, id_: int):
-    return client.get('/delete/{0}'.format(id_), follow_redirects=True)
+    return client.get("/delete/{0}".format(id_), follow_redirects=True)
 
 
 def updater(client, id_: int, content: str):
-    return client.post('/update/{0}'.format(id_),
+    return client.post("/update/{0}".format(id_),
                        data=dict(content=content), follow_redirects=True)
 
 
 def test_create_task(client):
     response = poster(client, "Yolo")
     assert response.status_code == 200
-    response = client.get('/')
-    assert b"Yolo" in response.data
+    response = client.get("/")
+    assert "Yolo" in response.text
     response = deleter(client, 1)
     assert response.status_code == 200
     response = deleter(client, 1)
     assert response.status_code == 404
-    response = client.get('/')
-    assert b"Yolo" not in response.data
+    response = client.get("/")
+    assert "Yolo" not in response.text
 
 
 def test_update(client):
@@ -37,5 +37,5 @@ def test_update(client):
     assert response.status_code == 200
     response = updater(client, 1, "Yolo_modified")
     assert response.status_code == 200
-    response = client.get('/')
-    assert b"Yolo_modified" in response.data
+    response = client.get("/")
+    assert "Yolo_modified" in response.text
